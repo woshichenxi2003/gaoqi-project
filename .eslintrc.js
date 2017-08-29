99e77339ce082c41c4f27c05baf497bf1aa2f31d
陈曦 <woshichenxi2003@2613.com>
2017-08-29 08:42:45 +0800

    去除部分页面

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 // http://eslint.org/docs/user-guide/configuring
-
+//先不用eslint
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    parserOptions: {
-        sourceType: 'module'
-    },
-    env: {
-        browser: true,
-    },
-    extends: 'vue',
-    // required to lint *.vue files
-    rules: {
-        "semi": [2, 'always'],
-        "eol-last": 0,
-        "indent": [2, 4]
-    },
-    plugins: [
-        'html'
-    ]
+    // root: true,
+    // parser: 'babel-eslint',
+    // parserOptions: {
+    //     sourceType: 'module'
+    // },
+    // env: {
+    //     browser: true,
+    // },
+    // extends: 'vue',
+    // // required to lint *.vue files
+    // rules: {
+    //     "semi": [2, 'always'],
+    //     "eol-last": 0,
+    //     "indent": [2, 4]
+    // },
+    // plugins: [
+    //     'html'
+    // ]
 }
